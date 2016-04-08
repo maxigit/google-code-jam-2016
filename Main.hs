25f2aac55f5351cb6a829c63fe9f59d558ae2e70
Maxime Bourget <bmx007@gmail.com>
2016-04-09 00:40:23 +0100

    First try. doesn't detect loop

--- Main.hs
+++ Main.hs
@@ -4,7 +4,11 @@ import Control.Monad
 import Control.Applicative
 import Data.Maybe
 import Data.List
-import Data.Map
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Monoid
+import Data.Set (Set)
+import qualified Data.Set  as Set
 
 main :: IO ()
 main = do
@@ -14,11 +18,17 @@ main = do
 
 solve f i = do 
   putStr$ "Case #" ++ show i ++ "# "
+
   n <- read <$> getLine
   putStrLn $ show (n :: Int)
 
+
 f :: Int -> String
-f _ = "INSOMNIA"
+f n = go n (Set.empty) 1 where
+  go _ _ 10 = "INSOMNIA"
+  go n done i | length (Set.toList done) == 10 = show $ i-1
+  go n done i = let numbers = show (n*i)
+     in go n (done <> Set.fromList numbers) (i+1)
   
 
 
