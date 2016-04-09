911e496c015c4bf80c82688805589ea584ea2e96
Maxime Bourget <bmx007@gmail.com>
2016-04-09 18:27:15 +0100

    Genenrate patterns

--- Main.hs
+++ Main.hs
@@ -14,50 +14,30 @@ import Debug.Trace
 main :: IO ()
 main = do
 	n <- read <$> getLine
-	mapM_ (solve ) [1..n]
+	mapM_ (solve f ) [1..n]
 
 
-solve i = do 
+solve f i = do 
   putStr$ "Case #" ++ show i ++ ": "
+  [k, c, s] <- map read . words <$> getLine
 
+  f k c s
 
-  putStrLn ""
-  [n, j] <- map read . words <$> getLine
-  mapM_ printJam (jamcoins n j)
-
-
-jamcoins :: Int -> Int -> [(Int, [Int])]
-jamcoins n j = take j coins
-  where coins = [ (i,  divs)
-                | i <- generateN n
-                , let divM = map divider (map (nToBase i) [2..10])
-		, divs <- maybeToList $ sequence divM
-                ]
-
-printJam (jam, divs) = do
- -- proof
- let ns = map (nToBase jam)  [2..10]
-     proof n d = do
-         let d' = n `div` d
-         print $ show n ++ " = " ++ show d ++ "*" ++ show d'
- -- zipWithM_ proof ns divs
- putStrLn (intercalate " " (map show (nToBase jam 10 :divs)))
+f :: Int -> Int -> Int -> IO ()
+f k c s = do
+ putStrLn (show (k, c, s))
  
 
+data Tile = L | G deriving (Show, Read, Eq)
 
-generateN :: Int -> [Int]
-generateN' n = [0..n'-1] where n' = 2 ^ n
--- fist and last number are one, so we don't need to generate them
-generateN n = map (\i -> 1+ 2*i + 2^(n-1)) (generateN' (n-2))
+nextGeneration :: [Tile] -> [Tile] -> [Tile] -> [Tile]
+nextGeneration original golds pat = concatMap next pat
+  where next L = original
+        next G = golds
 
--- Generate number b (as bits) in it's equivalent in base b
-nToBase 1 _ = 1
-nToBase 0 _ = 0
-nToBase n base = let r = n `mod` 2
-      in r + base * (nToBase (n `div` 2) base)
- 
 
--- find first dividers
-divider :: Int -> Maybe Int
-divider n = listToMaybe $  filter (\i -> n `mod` i == 0) (takeWhile (\i -> i*i <= n) [2..n-1])
-
+pattern :: [Tile] -> Int -> [Tile]
+pattern originals c =  go originals c
+  where go pat 1 = pat
+        go pat n = go (nextGeneration originals golds pat) (n-1)
+        golds = replicate (length originals) G
