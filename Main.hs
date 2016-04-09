f79d4203becf928fd8d72297b2fd1757efd8ae3b
Maxime Bourget <bmx007@gmail.com>
2016-04-09 22:57:26 +0100

    Rewrite Fractilize after doing some math.
    Much faster

--- Main.hs
+++ Main.hs
@@ -1,9 +1,9 @@
-module Main where
 
 import Control.Monad
 import Control.Applicative
 import Data.Maybe
 import Data.List
+import Data.List.Split
 import qualified Data.Map as Map
 import Data.Map (Map)
 import Data.Ord
@@ -24,53 +24,26 @@ solve f i = do
 
   f k c s
 
+-- a column is represented as fractal coordinate
+--in each index correspond to one generation
+--ex 1.2.3 for fist
 f :: Int -> Int -> Int -> IO ()
 f k c s = do
- -- putStrLn (show (k, c, s))
- let originalss = map (pattern c) (diag k)
-     ss = go originalss
-     go pat = case pick pat of
-                   Nothing ->  []
-                   Just (col, leftover) -> col : go leftover
+  -- we compute for each column which initial gold tiles it's supposed to check
+  -- each columns can check c initial gold tile at the same time
+  -- we need to check k columns and group the indexes by k
+  let indexes = chunksOf c [0..(k-1)]
 
- --putStrLn $ show originalss
- --putStrLn $ show ss
- if (length ss > s) 
- then putStrLn "IMPOSSIBLE"
- else putStrLn (intercalate " " $ map (show . (+1)) ss)
-
-diag :: Int -> [[Tile]]
-diag k = [ replicate i L ++ [G] ++ replicate (k-1-i) L | i <- [0..(k-1)]]
-
-data Tile = L | G deriving (Show, Read, Eq)
-
-nextGeneration :: [Tile] -> [Tile] -> [Tile] -> [Tile]
-nextGeneration original golds pat = concatMap next pat
-  where next L = original
-        next G = golds
+  if (length indexes) > s
+  then putStrLn "IMPOSSIBLE"
+  else  do -- we need to convert list of index to a column
+     let cols = map (show . toColumn k c) indexes
+     -- let cols = map (show  ) indexes
+     mapM_ putStr (intersperse " " cols)
+     putStrLn ""
 
 
-pattern :: Int -> [Tile] ->  [Tile]
-pattern c originals =  go originals c
-  where go pat 1 = pat
-        go pat n = go (nextGeneration originals golds pat) (n-1)
-        golds = replicate (length originals) G
-
--- Find for a list of patter the column with the more gold common in each patter
--- return ambigoous pattern
-
-pick :: [[Tile]] -> Maybe (Int, [[Tile]])
-pick [] = Nothing
-pick pats = let
- l = length (head pats)
- columns = Map.fromListWith (+)
-			    [ (i, 1)
-                            | i <- [0..(l-1)]
-                            , pat <- pats
-                            , pat !! i == G
-                            ]
- in case sortBy (comparing (Down . snd)) (Map.toList columns) of
-   [] -> Nothing
-   ((bestColumn, _): _) -> Just (bestColumn, filter (\pat -> pat !! bestColumn /= G) pats)
-
-
+toColumn :: Int -> Int -> [Int] -> Int
+toColumn k c is = let 
+  weights = map (k^) [c-1,c-2..0]
+  in 1 + (sum $ zipWith (*) is weights)
